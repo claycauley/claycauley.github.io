3d49d3290b3f30508ff5504c4ab7043c77ce1257
Clay <151966173+C1aYalC@users.noreply.github.com>
2026-02-03 13:42:11 -0500

    try to force an update

--- pokedex/service-worker.js
+++ pokedex/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'pokedex-v14';
-const API_CACHE_NAME = 'pokedex-api-v14';
-const IMAGE_CACHE_NAME = 'pokedex-images-v14';
+const CACHE_NAME = 'pokedex-v15';
+const API_CACHE_NAME = 'pokedex-api-v15';
+const IMAGE_CACHE_NAME = 'pokedex-images-v15';
 
 const urlsToCache = [
   '/',
