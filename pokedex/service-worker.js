1de89b7a200a7c81057f1d8fb13b7c2deccb45d5
Clay <151966173+C1aYalC@users.noreply.github.com>
2026-03-01 15:43:31 -0500

    styling changes fix mega name breaking lines

--- pokedex/service-worker.js
+++ pokedex/service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'pokedex-v47';
-const API_CACHE_NAME = 'pokedex-api-v47';
-const IMAGE_CACHE_NAME = 'pokedex-images-v47';
+const CACHE_NAME = 'pokedex-v48';
+const API_CACHE_NAME = 'pokedex-api-v48';
+const IMAGE_CACHE_NAME = 'pokedex-images-v48';
 
 const urlsToCache = [
   '/',
